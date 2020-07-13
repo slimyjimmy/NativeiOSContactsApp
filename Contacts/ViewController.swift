7e9c1afa9d11ebc903ea87bfcfbd38e2858c6d17
Djimon Nowak <dj.nowak@gmx.de>
2020-07-13 10:23:08 +0200

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Djimon Nowak on 13.07.20.
+//  Copyright © 2020 Djimon Nowak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
